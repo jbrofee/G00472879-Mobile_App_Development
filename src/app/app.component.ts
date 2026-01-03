834a3f1eeec93fc7315b3508355183dd82557c26
Jack <jack@novaphor.app>
2026-01-03 15:35:39 +0000

    Added settings link to toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-import {IonApp, IonContent, IonHeader, IonRouterOutlet, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+import {
+  IonApp, IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonRouterOutlet,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet, IonHeader, IonTitle, IonToolbar, IonContent],
+  imports: [IonApp, IonRouterOutlet, IonHeader, IonTitle, IonToolbar, IonContent, IonButtons, IonButton],
 })
 export class AppComponent {
   constructor() {}
